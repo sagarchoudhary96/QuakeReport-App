bd2c23c46cbb7852a622674234ff93e3c6a3c156
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2016-09-17 01:31:24 +0530

    Added Connection Checking Functionality

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -15,7 +15,10 @@
  */
 package com.example.android.quakereport;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -85,14 +88,25 @@ public class EarthquakeActivity extends AppCompatActivity implements android.app
             }
         });
 
+        //Check Whether Internet Connection is Available or Not
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo !=null && networkInfo.isConnected()){
 
-        // Get a reference to the LoaderManager, in order to interact with loaders.
-        android.app.LoaderManager loaderManager = getLoaderManager();
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            android.app.LoaderManager loaderManager = getLoaderManager();
 
-        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-        // because this activity implements the LoaderCallbacks interface).
-        loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+
+        } else {
+            mProgressBar.setVisibility(View.GONE);
+            emptyTextView.setText(R.string.no_internet);
+        }
+
+
 
 
     }
